116f7ffad26fad71523478a3454f8dec69e561fe
gvsax3322 <cosbe3822@gmail.com>
2024-03-27 12:08:32 +0900

    GA4적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 export const metadata: Metadata = {
   title: '전국 보건소 위치 안내',
   description: '전국 보건소 위치 안내 서비스 앱',
@@ -25,6 +26,7 @@ export default function RootLayout({
           rel="canonical"
           href="https://health-center-gvsax3322.vercel.app"
         ></link>
+        <GoogleAnalytics gaId="G-225423Q65G" />
       </head>
       <body>{children}</body>
     </html>
