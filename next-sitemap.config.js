6e980e01a6c511216613ba86d03fb016ed0ab4fe
gvsax3322 <cosbe3822@gmail.com>
2024-03-27 10:44:42 +0900

    네이버 검색 적용

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://health-center-gvsax3322.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
